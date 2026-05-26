73f3e3a733972a1b84781e54c4ac7fe0cd2a2642
Tapandat <166542132+Tapandat@users.noreply.github.com>
2026-05-26 16:34:55 +0530

    Add tables for users, expenses, and income

--- database.py
+++ database.py
@@ -1,16 +1,36 @@
-
 import sqlite3
 
 conn = sqlite3.connect("finance.db")
-
 cursor = conn.cursor()
 
+# Users table (email unique for Google OAuth)
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    email TEXT UNIQUE,
+    password BLOB
+)
+""")
+
+# Expenses table
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS expenses (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id TEXT,
     category TEXT,
     amount REAL,
     note TEXT
+)
+""")
+
+# Income table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS income (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id TEXT,
+    source TEXT,
+    amount REAL
 )
 """)
 
